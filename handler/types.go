b566caa2b7ed3c4d26e230aa315f41281db948e0
Aleksei <a.gryczewski999@gmail.com>
2020-01-10 16:14:45 -0500

    Started basis for command handling

--- handler/types.go
+++ handler/types.go
@@ -1,1 +1,16 @@
 package handler
+
+type Command struct {
+	Name string
+	Description string
+	Category string
+	Aliases []string
+	Usage string
+	Permissions []string
+}
+
+type CommandContext struct {
+	Prefix string
+	Args []string
+	Command string
+}
