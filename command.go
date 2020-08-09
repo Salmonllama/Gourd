a95181e372985dc319b3c6111ab9456295adef1e
Salmonllama <a.gryczewski999@gmail.com>
2020-08-09 02:10:34 -0400

    Moved elements to make more sense

--- command.go
+++ command.go
@@ -3,8 +3,9 @@ package gourd
 type Command struct {
 	Name        string
 	Description string
+	Aliases     []string
 	Inhibitor   interface{}
 	Arguments   []Argument // Can be nil -> no arguments required
-	Aliases     []string
+	Private     bool
 	Run         func(ctx CommandContext)
 }
