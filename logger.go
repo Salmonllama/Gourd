f13151d5752c8b4ec694bda752d3be89d42db44a
Salmonllama <a.gryczewski999@gmail.com>
2020-08-12 11:51:38 -0400

    Added logger interface and default logger

---
+++ logger.go
@@ -0,0 +1,30 @@
+package gourd
+
+import (
+	"fmt"
+	"time"
+)
+
+type Logger interface {
+	Debug(m ...interface{})
+	Info(m ...interface{})
+	Err(m ...interface{})
+}
+
+type ConsoleLogger struct {}
+var Console = ConsoleLogger{}
+
+func (ConsoleLogger) Debug(m ...interface{}) {
+	fmt.Printf("%v | DEBUG - ", time.Now().Clock())
+	fmt.Println(m)
+}
+
+func (ConsoleLogger) Info(m ...interface{}) {
+	fmt.Printf("%v | INFO - ", time.Now().Clock())
+	fmt.Println(m)
+}
+
+func (ConsoleLogger) Err(m ...interface{}) {
+	fmt.Printf("%v | Error - ", time.Now().Clock())
+	fmt.Println(m)
+}
